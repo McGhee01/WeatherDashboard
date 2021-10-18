0a7df4451fdbeb6ba4a2acf1bfc0b6dd2e84f659
Yah'Mir McGhee <yahmir.mcghee@gmail.com>
2021-10-17 23:04:38 -0400

    Variables for search cities has been created.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log ("Am I online?");
+
+//Declare a variable to store the searched city
+var city="";
+// variable declaration
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
